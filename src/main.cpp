a2d622d411029c6ce28c26aa0793b5352bc79b5f
BQuent1 <quentin.baud9@gmail.com>
2024-11-27 15:41:59 +0100

    implémentation IA simple aléatoire

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "Player.h"
-#include <array>
 #include <cstdlib>
 #include <ctime>
+#include <array>
+#include <string>
 
 Player create_player(std::string player_name, char player_symbol)
 {
@@ -34,9 +35,6 @@ void draw_game_board(std::array<char, 9> tab_joueur)
     std::cout << "\n\n";
 }
 
-#include <array>
-#include <string>
-
 bool is_game_over(std::array<char, 9> tab)
 {
     for (int i = 0; i < 9; i += 3)
@@ -64,6 +62,11 @@ bool is_game_over(std::array<char, 9> tab)
         return true;
     }
 
+    return false;
+}
+
+bool is_match_nul(std::array<char, 9> tab)
+{
     for (int i = 0; i < 9; i++)
     {
         if (tab[i] == ' ')
@@ -71,76 +74,158 @@ bool is_game_over(std::array<char, 9> tab)
             return false;
         }
     }
-    
-    std::cout << "\nMatch nul !\n";
+
     return true;
 }
 
-void mode_deux_joueurs(Player player1, Player player2, std::array <char, 9> tab)
+void game_start(Player player1, Player player2, std::array<char, 9> tab, int mode)
 {
-    draw_game_board(tab);
-
     std::srand(static_cast<unsigned>(std::time(0)));
     char symbol_playing{};
     Player current_player{};
+    int current_player_index{};
 
     int kicommence = 1 + (std::rand() % 2);
     if (kicommence == 1)
     {
-        std::cout << "\n" << player1.name << " tu commences\n";
+        std::cout << "\n"
+                  << player1.name << " tu commences\n";
         current_player = player1;
+        current_player_index = 1;
     }
+
     else
     {
-        std::cout << "\n" << player2.name << " tu commences\n";
-        current_player = player2;
+        if (mode == 1) // si mode de jeu a deux
+        {
+            std::cout << "\n"
+                      << player2.name << " tu commences\n";
+            current_player = player1;
+            current_player_index = 2;
+        }
+        else // mode solo
+        {
+            std::cout << "\nL'IA commence";
+            current_player = player2;
+            current_player_index = 2;
+        }
     }
 
     draw_game_board(tab);
 
-    while (!is_game_over(tab))
+    while (!is_game_over(tab) and !is_match_nul(tab))
     {
-        std::string pos;
-        std::cout << current_player.name << ", entre une position (ex: A1) : ";
-        std::cin >> pos;
 
-        int index = -1;
-        if (pos == "A1")
-            index = 0;
-        else if (pos == "B1")
-            index = 1;
-        else if (pos == "C1")
-            index = 2;
-        else if (pos == "A2")
-            index = 3;
-        else if (pos == "B2")
-            index = 4;
-        else if (pos == "C2")
-            index = 5;
-        else if (pos == "A3")
-            index = 6;
-        else if (pos == "B3")
-            index = 7;
-        else if (pos == "C3")
-            index = 8;
-
-        if (index == -1 || tab[index] != ' ')
+        if (mode == 1)
         {
-            std::cout << "\nPosition invalide ou deja occupee, reessaye.\n";
-            continue;
+            std::string pos;
+            std::cout << current_player.name << ", entre une position (ex: A1) : ";
+            std::cin >> pos;
+
+            int index = -1;
+            if (pos == "A1")
+                index = 0;
+            else if (pos == "B1")
+                index = 1;
+            else if (pos == "C1")
+                index = 2;
+            else if (pos == "A2")
+                index = 3;
+            else if (pos == "B2")
+                index = 4;
+            else if (pos == "C2")
+                index = 5;
+            else if (pos == "A3")
+                index = 6;
+            else if (pos == "B3")
+                index = 7;
+            else if (pos == "C3")
+                index = 8;
+
+            if (index == -1 || tab[index] != ' ')
+            {
+                std::cout << "\nPosition invalide ou deja occupee, reessaye.\n";
+                continue;
+            }
+
+            tab[index] = current_player.symbole;
+
+            draw_game_board(tab);
+
+            if (current_player.name == player1.name)
+            {
+                current_player = player2;
+                current_player_index = 2;
+            }
+            else
+            {
+                current_player = player1;
+                current_player_index = 1;
+            }
         }
 
-        tab[index] = current_player.symbole;
-
-        draw_game_board(tab);
-
-        if (current_player.name == player1.name)
+        else // mode solo
         {
-            current_player = player2;
-        }
-        else
-        {
-            current_player = player1;
+            if (current_player_index == 2)
+            {
+                std::cout << "Reflexion en cours...";
+                int random_index = 1 + (std::rand() % 9);
+                while (tab[random_index] != ' ')
+                {
+                    random_index = 1 + (std::rand() % 9);
+                }
+                tab[random_index] = current_player.symbole;
+
+                draw_game_board(tab);
+            }
+            else
+            {
+                std::string pos;
+                std::cout << current_player.name << ", entre une position (ex: A1) : ";
+                std::cin >> pos;
+
+                int index = -1;
+                if (pos == "A1")
+                    index = 0;
+                else if (pos == "B1")
+                    index = 1;
+                else if (pos == "C1")
+                    index = 2;
+                else if (pos == "A2")
+                    index = 3;
+                else if (pos == "B2")
+                    index = 4;
+                else if (pos == "C2")
+                    index = 5;
+                else if (pos == "A3")
+                    index = 6;
+                else if (pos == "B3")
+                    index = 7;
+                else if (pos == "C3")
+                    index = 8;
+
+                if (index == -1 || tab[index] != ' ')
+                {
+                    std::cout << "\nPosition invalide ou deja occupee, reessaye.\n";
+                    continue;
+                }
+
+                tab[index] = current_player.symbole;
+
+                draw_game_board(tab);
+            }
+            
+            
+            if (current_player.name == player1.name)
+            {
+                current_player = player2;
+                current_player_index = 2;
+            }
+            else
+            {
+                current_player = player1;
+                current_player_index = 1;
+            }
         }
     }
 
@@ -148,12 +233,20 @@ void mode_deux_joueurs(Player player1, Player player2, std::array <char, 9> tab)
     {
         if (current_player.name == player1.name)
         {
-            std::cout << "\n" << player2.name << " tu as gagne, bien ouej\n";
+            std::cout << "\n"
+                      << player2.name << " a gagne, bien ouej\n";
         }
-        else if(current_player.name == player2.name)
+        else if (current_player.name == player2.name)
         {
-            std::cout << "\n" << player1.name << " tu as gagne, bien ouej\n";
+            std::cout << "\n"
+                      << player1.name << " a gagne, bien ouej\n";
         }
+    }
+
+    else if (is_match_nul(tab))
+    {
+        std::cout << "\nMatch nul !";
+        return;
     }
 }
 
@@ -164,7 +257,7 @@ int main()
     std::cout << "Bienvenue dans le jeu du TicTacToe \nVeuillez choisir un mode de jeu : \n1. Deux joueurs\n2. Un joueur contre l'IA\n";
     std::cin >> mode_jeu;
 
-    if (mode_jeu == 1) //mode de jeu multi-joueurs
+    if (mode_jeu == 1) // mode de jeu multi-joueurs
     {
         std::string nom_joueur_1{};
         std::string nom_joueur_2{};
@@ -181,9 +274,28 @@ int main()
         std::cout << nom_joueur_2 << ", quel symbole prends tu ?\n";
         std::cin >> symbole_joueur_2;
 
-        mode_deux_joueurs(create_player(nom_joueur_1,symbole_joueur_1), create_player(nom_joueur_2,symbole_joueur_2), tab);
+        game_start(create_player(nom_joueur_1, symbole_joueur_1), create_player(nom_joueur_2, symbole_joueur_2), tab, mode_jeu);
     }
 
+    else // mode de jeu solo
+    {
+        std::string nom_joueur_1{};
+        char symbole_joueur_1{};
+        char symbole_ia{};
+        std::cout << "Joueur 1, quel est ton nom ?\n";
+        std::cin >> nom_joueur_1;
+        std::cout << nom_joueur_1 << ", quel symbole prends tu ?\n";
+        std::cin >> symbole_joueur_1;
+
+        if(symbole_joueur_1 == 'X'){
+            symbole_ia = 'O';
+        }
+        else{
+            symbole_ia = 'O';
+        }
+
+        game_start(create_player(nom_joueur_1, symbole_joueur_1), create_player("IA", symbole_ia), tab, mode_jeu);
+    }
 
     return 0;
 }
